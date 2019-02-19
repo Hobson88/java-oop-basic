5f0258b3cf0455148ccfa3cb2dc39d21dbb7d52f
Hobson88 <krawczakrobert@gmail.com>
2019-02-19 10:18:36 +0100

    user2

--- src/UserMain.java
+++ src/UserMain.java
@@ -6,9 +6,9 @@ import java.time.LocalDateTime;
 public class UserMain {
     public static void main(String[] args) {
         UserController uc = new UserController();
-        uc.addUser("Robert", "Krawczak","hobson88", "1234","rob@t.pl", "700111700", LocalDate.of(1988,2,26), LocalDateTime.now());
-        uc.addUser("Magdalena", "Krawczak","cudownaZonka", "0000","m@m.mk", "700111701", LocalDate.of(1993,10,23), LocalDateTime.now());
-        uc.addUser("Franciszek", "Krawczak","synek", "kdi3djnjcnvnv","f@m.mk", "700111702", LocalDate.of(2017,12,15), LocalDateTime.now());
+        uc.addUser("Robert", "Krawczak","hobson88", "1234","rob@t.pl", "700111700", LocalDate.of(1944,2,2), LocalDateTime.now());
+        uc.addUser("Magdalena", "Krawczak","cudownaZonka", "0000","m@m.mk", "700111701", LocalDate.of(1993,12,3), LocalDateTime.now());
+        uc.addUser("Franciszek", "Krawczak","synek", "kdi3djnjcnvnv","f@m.mk", "700111702", LocalDate.of(2017,12,11), LocalDateTime.now());
         uc.getAllUsers();
     }
 
